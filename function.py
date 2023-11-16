424b1a3b45e540ffa108af02527eddc8a42324d5
LTommy07 <z.tommy.lim@gmail.com>
2023-11-16 16:03:52 +0100

    g

--- function.py
+++ function.py
@@ -3,7 +3,6 @@ def list_of_files(directory, extension):
     files_names = []
     for filename in os.listdir(directory):
         if filename.endswith(extension):
-
             files_names.append(filename)
     return files_names
 
