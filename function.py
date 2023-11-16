da9f99bd7b04127ee04624a65b1a6c82dd848521
LTommy07 <z.tommy.lim@gmail.com>
2023-11-16 16:03:00 +0100

    g

--- function.py
+++ function.py
@@ -1,16 +1,19 @@
 import os
 def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
 
- files_names = []
- for filename in os.listdir(directory):
-    if filename.endswith(extension):
-        files_names.append(filename)
-        return files_names
+            files_names.append(filename)
+    return files_names
+
+def prenom(prenom):
 
 
 
-def minuscule (nom_fichier):
-    for i in nom_fichier:
+
+def minuscule (files_names):
+    for i in files_names:
         ascii = ord(i)
         min = chr(ascii)
         if 65 <= ascii <= 90:
