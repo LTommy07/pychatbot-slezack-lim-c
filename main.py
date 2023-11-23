1cebf90b58a0644b1f2f3f0b6d4f46f5f7e00087
Noam Slezack <145605933+nonolasaire@users.noreply.github.com>
2023-11-23 17:19:05 +0100

    a

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functiontest import *
+from function import *
 directory= "./speeches"
 file_names =list_of_files(directory,"txt")
 print("Fichiers trouvés dans le répertoire 'speeches':")
@@ -6,7 +6,7 @@ print("Fichiers trouvés dans le répertoire 'speeches':")
 for name in file_names:
     print(name)
 
-print(nom_president)
+
 nom_president=extraire_noms_presidents(directory)
 print("\nListe des présidents")
 afficher_noms_presidents(nom_president)
