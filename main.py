8782fe6ad77d03f762f3c90aafc2837371abac08
LTommy07 <150531620+LTommy07@users.noreply.github.com>
2023-11-24 00:10:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from function import *
-directory= "./speeches"
-file_names =list_of_files(directory,"txt")
+directory = "./speeches"
+file_names = list_of_files(directory,"txt")
 print("Fichiers trouvés dans le répertoire 'speeches':")
 
 for name in file_names:
     print(name)
 
 
-nom_president=extraire_noms_presidents(directory)
+nom_president = extraire_noms_presidents(directory)
 print("\nListe des présidents")
 afficher_noms_presidents(nom_president)
 
 print(nom_president)
 
-
+remove_accents(file_names)
+minus(file_names)
 
 
 
