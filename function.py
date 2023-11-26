8764179d4f79e079a8cffb0f78224d49345bca33
LTommy07 <150531620+LTommy07@users.noreply.github.com>
2023-11-26 19:34:15 +0100

    Update function.py

--- function.py
+++ function.py
@@ -6,13 +6,11 @@ import math
 
 def list_of_files(directory, extension):
     """
-     parcourt un répertoire donné et compile une liste de tous les fichiers qui se terminent par une extension spécifiée.
+     Parcourt un répertoire donné et compile une liste de tous les fichiers qui se terminent par une extension spécifiée.
     """
     files_names = []
-    # Parcours du répertoire.
-    for filename in os.listdir(directory):
-        # Vérification de l'extension du fichier.
-        if filename.endswith(extension):
+    for filename in os.listdir(directory):    # Parcours du répertoire.
+        if filename.endswith(extension):    # Vérification de l'extension du fichier.
             files_names.append(filename)
     return files_names
 
@@ -20,16 +18,13 @@ def list_of_files(directory, extension):
 
 def extraire_noms_presidents(file_names):
     """
-     extrait les noms des présidents à partir des noms de fichiers en supprimant les numéros et les extensions.
+     Extrait les noms des présidents à partir des noms de fichiers en supprimant les numéros et les extensions.
     """
-    # Ensemble pour éviter les doublons.
-    presidents = set()
+    presidents = set()    # Ensemble pour éviter les doublons.
     for name in file_names:
-        # Extraction du nom en enlevant l'extension et en séparant par '_'.
-        parts = name.replace('.txt', '').split('_')
+        parts = name.replace('.txt', '').split('_')    # Extraction du nom en enlevant l'extension et en séparant par '_'.
         if len(parts) > 1:
-            # Retrait des chiffres et ajout du nom nettoyé à l'ensemble.
-            nom_president = ''.join([char for char in parts[1] if char.isalpha()])
+            nom_president = ''.join([char for char in parts[1] if char.isalpha()])    # Retrait des chiffres et ajout du nom nettoyé à l'ensemble.
             presidents.add(nom_president)
     return list(presidents)
 
@@ -37,8 +32,7 @@ def extraire_noms_presidents(file_names):
 
 def associer_prenoms_presidents(presidents):
     """
-     associe un prénom à chaque nom de président .
-
+     Associe un prénom à chaque nom de président 
     """
     # Dictionnaire de correspondance prénom-nom.
     prenoms_presidents = {
@@ -49,15 +43,14 @@ def associer_prenoms_presidents(presidents):
         'Sarkozy': 'Nicolas',
         'Hollande': 'François'
     }
-    # Construction du dictionnaire final.
-    return {president: prenoms_presidents.get(president, 'PrénomInconnu') for president in presidents}
+    
+    return {president: prenoms_presidents.get(president, 'PrénomInconnu') for president in presidents}    # Construction du dictionnaire final.
 
 
 
 def afficher_presidents(presidents):
     """
-     affiche les noms et prénoms des présidents.
-
+     Affiche les noms et prénoms des présidents.
     """
     for nom_famille, prenom in presidents.items():
         print(f"{prenom} {nom_famille}")
@@ -66,27 +59,22 @@ def afficher_presidents(presidents):
 
 def convertir_en_minuscules(directory, target_directory):
     """
-     convertit tous les textes des fichiers d'un répertoire en minuscules et les sauvegarde dans un nouveau répertoire.
-
+     Convertit tous les textes des fichiers d'un répertoire en minuscules et les sauvegarde dans un nouveau répertoire.
     """
-    # Création du répertoire cible si nécessaire.
-    os.makedirs(target_directory, exist_ok=True)
+    os.makedirs(target_directory, exist_ok=True)    # Création du répertoire cible si nécessaire.
     for filename in os.listdir(directory):
         if filename.endswith(".txt"):
             new_filename = os.path.join(target_directory, filename)
             with open(os.path.join(directory, filename), 'r', encoding='utf-8') as f1, \
                     open(new_filename, 'w', encoding='utf-8') as f2:
-                # Lecture et conversion en minuscules.
-                content = f1.read().lower()
-                # Écriture dans le nouveau fichier.
-                f2.write(content)
+                content = f1.read().lower()    # Lecture et conversion en minuscules.
+                f2.write(content)    # Écriture dans le nouveau fichier.
 
 
 
 def retirer_accents(texte):
     """
-     remplace les lettres accentuées d'un texte par leur équivalent sans accent.
-
+     Remplace les lettres accentuées d'un texte par leur équivalent sans accent.
     """
     # Dictionnaire de correspondance des accents.
     correspondances = {
@@ -97,14 +85,14 @@ def retirer_accents(texte):
         'ù': 'u', 'û': 'u', 'ü': 'u',
         'ç': 'c',
     }
-    # Remplacement des caractères accentués.
-    return ''.join(correspondances.get(c, c) for c in texte)
+    
+    return ''.join(correspondances.get(c, c) for c in texte)    # Remplacement des caractères accentués.
 
 
 def supprimer_ponctuation_et_accents(directory):
     """
-    Parcourt chaque fichier dans le répertoire spécifié, supprime la ponctuation et les accents,
-    et sauvegarde les changements dans le même fichier.
+     Parcourt chaque fichier dans le répertoire spécifié, supprime la ponctuation et les accents,
+     et sauvegarde les changements dans le même fichier.
     """
     punctuation = string.punctuation.replace("'", "").replace("-", "")  # Conserve les apostrophes et les tirets
 
@@ -115,15 +103,12 @@ def supprimer_ponctuation_et_accents(directory):
             with open(file_path, 'r', encoding='utf-8') as file:
                 content = file.read()
 
-                # Suppression de la ponctuation
-                for char in punctuation:
+                for char in punctuation:    # Suppression de la ponctuation
                     content = content.replace(char, " ")
 
-                # Remplacement des apostrophes et tirets
-                content = content.replace("'", " ").replace("-", " ")
+                content = content.replace("'", " ").replace("-", " ")    # Remplacement des apostrophes et tirets
 
-                # Suppression des accents
-                content = retirer_accents(content)
+                content = retirer_accents(content)    # Suppression des accents
 
             with open(file_path, 'w', encoding='utf-8') as file:
                 file.write(content)
@@ -131,16 +116,12 @@ def supprimer_ponctuation_et_accents(directory):
 
 def calculer_tf(texte):
     """
-     calcule le nombre d'occurrences de chaque mot dans un texte (TF).
-
+     Calcule le nombre d'occurrences de chaque mot dans un texte (TF).
     """
-    # Séparation du texte en mots.
-    mots = texte.split()
-    # Dictionnaire pour stocker les fréquences.
-    tf = {}
+    mots = texte.split()    # Séparation du texte en mots.
+    tf = {}    # Dictionnaire pour stocker les fréquences.
     for mot in mots:
-        # Incrémentation de la fréquence pour chaque mot.
-        if mot in tf:
+        if mot in tf:    # Incrémentation de la fréquence pour chaque mot.
             tf[mot] += 1
         else:
             tf[mot] = 1
@@ -150,51 +131,37 @@ def calculer_tf(texte):
 
 def calculer_idf(directory):
     """
-     calcule l'IDF pour chaque mot unique dans un ensemble de fichiers.
-
+     Calcule l'IDF pour chaque mot unique dans un ensemble de fichiers.
     """
-    # Liste des fichiers dans le répertoire.
-    files = os.listdir(directory)
-    # Nombre total de fichiers.
-    nb_fichiers = len([f for f in files if f.endswith('.txt')])
-    # Dictionnaire pour compter combien de documents contiennent chaque mot.
-    comptage_docs_mot = {}
+    files = os.listdir(directory)    # Liste des fichiers dans le répertoire.
+    nb_fichiers = len([f for f in files if f.endswith('.txt')])    # Nombre total de fichiers.
+    comptage_docs_mot = {}    # Dictionnaire pour compter combien de documents contiennent chaque mot.
     for filename in files:
         if filename.endswith('.txt'):
             with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
-                # Ensemble des mots uniques dans le document.
-                mots_uniques = set(file.read().split())
+                mots_uniques = set(file.read().split())    # Ensemble des mots uniques dans le document.
                 for mot in mots_uniques:
-                    # Incrémentation du comptage pour chaque mot unique.
-                    comptage_docs_mot[mot] = comptage_docs_mot.get(mot, 0) + 1
-    # Dictionnaire pour stocker les scores IDF.
-    idf = {}
+                    comptage_docs_mot[mot] = comptage_docs_mot.get(mot, 0) + 1    # Incrémentation du comptage pour chaque mot unique.
+    idf = {}    # Dictionnaire pour stocker les scores IDF.
     for mot, comptage in comptage_docs_mot.items():
-        # Calcul du score IDF en évitant la division par zéro.
-        idf[mot] = math.log(nb_fichiers / float(comptage)) if comptage > 0 else 0
+        idf[mot] = math.log(nb_fichiers / float(comptage)) if comptage > 0 else 0    # Calcul du score IDF en évitant la division par zéro.
     return idf
 
 
 
 def calculer_tf_idf(directory):
     """
-     calcule la matrice TF-IDF pour tous les fichiers d'un répertoire.
-
+     Calcule la matrice TF-IDF pour tous les fichiers d'un répertoire.
     """
-    # Dictionnaire pour la matrice TF-IDF.
-    tf_idf_matrice = {}
-    # Calcul des scores IDF.
-    idf_scores = calculer_idf(directory)
-    # Liste des fichiers.
-    files = os.listdir(directory)
+    tf_idf_matrice = {}    # Dictionnaire pour la matrice TF-IDF.
+    idf_scores = calculer_idf(directory)    # Calcul des scores IDF.
+    files = os.listdir(directory)    # Liste des fichiers.
     for filename in files:
         if filename.endswith('.txt'):
             with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
-                # Calcul des scores TF pour le fichier actuel.
-                tf_scores = calculer_tf(file.read())
+                tf_scores = calculer_tf(file.read())    # Calcul des scores TF pour le fichier actuel.
                 for mot, tf in tf_scores.items():
-                    # Calcul du score TF-IDF.
-                    tf_idf = tf * idf_scores[mot]
+                    tf_idf = tf * idf_scores[mot]    # Calcul du score TF-IDF.
                     if mot in tf_idf_matrice:
                         tf_idf_matrice[mot].append(tf_idf)
                     else:
@@ -206,8 +173,7 @@ def calculer_tf_idf(directory):
 
 def trouver_mots_moins_importants(tf_idf_matrice):
     """
-     identifie les mots qui ont un score TF-IDF de zéro ou très bas dans tous les documents.
-
+     Identifie les mots qui ont un score TF-IDF de zéro ou très bas dans tous les documents.
     """
 
     return [mot for mot, scores in tf_idf_matrice.items() if all(score == 0 for score in scores)]
@@ -216,15 +182,12 @@ def trouver_mots_moins_importants(tf_idf_matrice):
 
 def trouver_mots_avec_tf_idf_le_plus_eleve(tf_idf_matrice):
     """
-    Cette fonction identifie les mots ayant le score TF-IDF le plus élevé dans l'ensemble des documents.
-
+     Cette fonction identifie les mots ayant le score TF-IDF le plus élevé dans l'ensemble des documents.
     """
-    # Initialisation du score maximal et de la liste des mots correspondants.
-    score_tf_idf_maximal = 0
+    score_tf_idf_maximal = 0    # Initialisation du score maximal et de la liste des mots correspondants.
     liste_mots_avec_score_maximal = []
     for mot, scores in tf_idf_matrice.items():
-        # Trouver le score maximal pour chaque mot.
-        max_score = max(scores)
+        max_score = max(scores)    # Trouver le score maximal pour chaque mot.
         if max_score > score_tf_idf_maximal:
             score_tf_idf_maximal = max_score
             liste_mots_avec_score_maximal = [mot]
@@ -233,49 +196,36 @@ def trouver_mots_avec_tf_idf_le_plus_eleve(tf_idf_matrice):
     return liste_mots_avec_score_maximal, score_tf_idf_maximal
 
 
-# Fonction pour identifier les mots les plus répétés par un président.
 def mots_les_plus_repetes_par_president(tf_idf_matrice, directory, nom_president):
     """
-    identifie les mots les plus répétés dans les discours d'un président donné, en tenant compte de leur importance (TF-IDF).
-
+     Identifie les mots les plus répétés dans les discours d'un président donné, en tenant compte de leur importance (TF-IDF).
     """
-    # Ensemble des mots avec un score TF-IDF positif.
-    mots_avec_tfidf_positif = set(mot for mot, scores in tf_idf_matrice.items() if any(score > 0.1 for score in scores))
-    # Dictionnaire pour le comptage des mots.
-    comptage_mots = {}
-    # Liste des fichiers correspondant au président.
-    president_files = [filename for filename in os.listdir(directory) if nom_president in filename]
+    mots_avec_tfidf_positif = set(mot for mot, scores in tf_idf_matrice.items() if any(score > 0.1 for score in scores))    # Ensemble des mots avec un score TF-IDF positif.
+    comptage_mots = {}    # Dictionnaire pour le comptage des mots.
+    president_files = [filename for filename in os.listdir(directory) if nom_president in filename]    # Liste des fichiers correspondant au président.
     for filename in president_files:
         with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
-            # Lecture des mots du fichier.
-            mots = file.read().split()
+            mots = file.read().split()    # Lecture des mots du fichier.
             for mot in mots:
                 if mot in mots_avec_tfidf_positif:
                     comptage_mots[mot] = comptage_mots.get(mot, 0) + 1
-    # Tri des mots par nombre d'occurrences et récupération des vingt premiers.
-    vingt_mots_les_plus_repetes = sorted(comptage_mots.items(), key=lambda x: x[1], reverse=True)[:20]
+    vingt_mots_les_plus_repetes = sorted(comptage_mots.items(), key=lambda x: x[1], reverse=True)[:20]    # Tri des mots par nombre d'occurrences et récupération des vingt premiers.
     return vingt_mots_les_plus_repetes
 
 
-# Fonction pour compter les mentions du mot "nation".
+
 def compter_mentions_nation(directory):
     """
-     compte le nombre de fois que le mot "nation" est mentionné par chaque président dans l'ensemble des documents.
-
+     Compte le nombre de fois que le mot "nation" est mentionné par chaque président dans l'ensemble des documents.
     """
-    # Dictionnaire pour les mentions de "nation".
-    mentions_nation = {}
-    # Le mot à rechercher.
-    mot_recherche = 'nation'
+    mentions_nation = {}    # Dictionnaire pour les mentions de "nation".
+    mot_recherche = 'nation'    # Le mot à rechercher.
     for filename in os.listdir(directory):
         if filename.endswith(".txt"):
-            # Extraction du nom du président sans numéro.
-            president = extraire_nom_president2(filename)
+            president = extraire_nom_president2(filename)    # Extraction du nom du président sans numéro.
             with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
-                # Lecture des mots du fichier.
-                content = file.read().split()
-                # Comptage des occurrences de "nation".
-                mentions = content.count(mot_recherche)
+                content = file.read().split()    # Lecture des mots du fichier.
+                mentions = content.count(mot_recherche)    # Comptage des occurrences de "nation".
                 if mentions > 0:
                     if president in mentions_nation:
                         mentions_nation[president] += mentions
@@ -287,23 +237,18 @@ def compter_mentions_nation(directory):
 
 def extraire_nom_president2(filename):
     """
-     extrait le nom du président à partir du nom d'un fichier en supprimant les numéros et les caractères non alphabétiques.
-
+     Extrait le nom du président à partir du nom d'un fichier en supprimant les numéros et les caractères non alphabétiques.
     """
-    # Suppression de l'extension et division basée sur '_'.
-    parts = filename.replace(".txt", "").split('_')
+    parts = filename.replace(".txt", "").split('_')    # Suppression de l'extension et division basée sur '_'.
     nom_president = ""
     if len(parts) > 1:
-        # Reconstruction du nom du président.
-        nom_president = '_'.join(parts[1:])
-        # Suppression des chiffres.
-        nom_president = ''.join([i for i in nom_president if not i.isdigit()])
+        nom_president = '_'.join(parts[1:])    # Reconstruction du nom du président.
+        nom_president = ''.join([i for i in nom_president if not i.isdigit()])    # Suppression des chiffres.
     return nom_president.strip()
 
 def trouver_premier_president_climat_ecologie(directory):
     """
-     détermine le premier président à mentionner des termes liés au climat ou à l'écologie en suivant l'ordre d'investiture.
-
+     Détermine le premier président à mentionner des termes liés au climat ou à l'écologie en suivant l'ordre d'investiture.
     """
     # Liste des termes liés au climat ou à l'écologie.
     mots_recherches = [
@@ -315,6 +260,7 @@ def trouver_premier_president_climat_ecologie(directory):
     ]
     president_premiere_mention = ""
     fichier_premiere_mention = None
+    
     # Ordre d'investiture des présidents.
     ordre_investiture = [
         'Giscard dEstaing', 'Chirac1', 'Chirac2', 'Mitterrand1', 'Mitterrand2',
@@ -335,24 +281,19 @@ def trouver_premier_president_climat_ecologie(directory):
 
 def mots_communs_tous_presidents(directory, tf_idf_matrice):
     """
-     trouve les mots qui sont communs à tous les discours des présidents, à l'exception de ceux ayant un score TF-IDF de zéro.
-
+     Trouve les mots qui sont communs à tous les discours des présidents, à l'exception de ceux ayant un score TF-IDF de zéro.
     """
-    # Liste des fichiers dans le répertoire.
-    files = [f for f in os.listdir(directory) if f.endswith('.txt')]
+    files = [f for f in os.listdir(directory) if f.endswith('.txt')]    # Liste des fichiers dans le répertoire.
     if not files:
         return []
 
-    # Initialisation des mots communs avec ceux du premier fichier.
-    with open(os.path.join(directory, files[0]), 'r', encoding='utf-8') as file:
+    with open(os.path.join(directory, files[0]), 'r', encoding='utf-8') as file:    # Initialisation des mots communs avec ceux du premier fichier.
         mots_communs = set(file.read().split())
 
-    # Intersection des mots communs avec ceux des fichiers suivants.
-    for filename in files[1:]:
+    for filename in files[1:]:    # Intersection des mots communs avec ceux des fichiers suivants.
         with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
             mots_fichier = set(file.read().split())
             mots_communs = mots_communs.intersection(mots_fichier)
 
-    # Filtre pour exclure les mots avec un score TF-IDF de zéro.
-    mots_communs = {mot for mot in mots_communs if any(score > 0 for score in tf_idf_matrice.get(mot, []))}
+    mots_communs = {mot for mot in mots_communs if any(score > 0 for score in tf_idf_matrice.get(mot, []))}    # Filtre pour exclure les mots avec un score TF-IDF de zéro.
     return mots_communs
